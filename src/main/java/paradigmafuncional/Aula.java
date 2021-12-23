5d63a454b1c2587bd62a4c2c2308aa18f77e2ff1
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-12-23 09:07:24 -0300

    First Commit

---
+++ src/main/java/paradigmafuncional/Aula.java
@@ -0,0 +1,9 @@
+package paradigmafuncional;
+
+public class Aula {
+    public static void main(String[] args) {
+        int valor = 10;
+        int resultado = valor * 3;
+        System.out.println("O resultado é:: " + resultado);
+    }
+}
