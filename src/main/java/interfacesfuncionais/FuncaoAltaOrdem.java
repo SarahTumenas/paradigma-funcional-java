05e4af4f7fb6729d00b9653f6b8ea2e550c73431
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-12-23 15:09:20 -0300

    Função de Alta ordem

--- src/main/java/interfacesfuncionais/FuncaoAltaOrdem.java
+++ src/main/java/interfacesfuncionais/FuncaoAltaOrdem.java
@@ -3,7 +3,14 @@ package interfacesfuncionais;
 public class FuncaoAltaOrdem {
     public static void main(String[] args) {
         Calculo soma = (a, b) -> a + b;
-        System.out.println(executarOperacao(soma,15,25));
+        Calculo subtracao = (a, b) -> a - b;
+        Calculo divisao= (a, b) -> a * b;
+        Calculo multiplicacao = (a, b) -> a / b;
+
+        System.out.println( "soma: "+ executarOperacao(soma,10,5) +"\n"
+                + "subtração: " + executarOperacao(subtracao, 10,5) +"\n"
+                + "divisão: "+ executarOperacao(divisao, 10, 5)+"\n"
+                + "multiplicação: " + executarOperacao(multiplicacao, 10,5));
     }
 
     public static int executarOperacao (Calculo calculo, int a , int b){
