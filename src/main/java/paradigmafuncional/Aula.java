56a2caa3afaa32e92a72d3f07cda311e142bad40
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-12-23 09:27:00 -0300

    paradigma funcional

--- src/main/java/paradigmafuncional/Aula.java
+++ src/main/java/paradigmafuncional/Aula.java
@@ -1,9 +1,24 @@
 package paradigmafuncional;
-//paradigma imperativo
+
+import java.util.function.UnaryOperator;
+
 public class Aula {
+
+
     public static void main(String[] args) {
+        /*paradigma imperativo
         int valor = 10;
         int resultado = valor * 3;
         System.out.println("O resultado é:: " + resultado);
+        */
+
+        //paradigma funcional
+        UnaryOperator <Integer> calcularValorVezesTres = valor -> valor*3;
+        int valor = 10;
+        System.out.println("O resultado é:: " + calcularValorVezesTres.apply(10));
     }
+
+
+
+
 }
