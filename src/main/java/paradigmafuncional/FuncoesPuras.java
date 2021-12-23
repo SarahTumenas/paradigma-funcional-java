7593ff281513e8b6ab5f2a791f5fad639a410422
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-12-23 10:27:49 -0300

    Imutabilidade

--- src/main/java/paradigmafuncional/FuncoesPuras.java
+++ src/main/java/paradigmafuncional/FuncoesPuras.java
@@ -6,6 +6,7 @@ public class FuncoesPuras {
     public static void main(String[] args) {
         BiPredicate <Integer,Integer> verificarSeMaior =
                 (parametro, valorComparacao) -> parametro > valorComparacao;
+        //devem sempre retornar o mesmo valor, quantas vezes forem chamadas
         System.out.println(verificarSeMaior.test(13,12));
         System.out.println(verificarSeMaior.test(13,12));
         System.out.println(verificarSeMaior.test(12,13));
