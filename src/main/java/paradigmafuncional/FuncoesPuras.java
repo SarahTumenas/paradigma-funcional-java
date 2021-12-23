ec9b8d267a75e63f429a6f83549f60c18e3c71f6
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-12-23 10:18:58 -0300

    Funções Puras

---
+++ src/main/java/paradigmafuncional/FuncoesPuras.java
@@ -0,0 +1,14 @@
+package paradigmafuncional;
+
+import java.util.function.BiPredicate;
+
+public class FuncoesPuras {
+    public static void main(String[] args) {
+        BiPredicate <Integer,Integer> verificarSeMaior =
+                (parametro, valorComparacao) -> parametro > valorComparacao;
+        System.out.println(verificarSeMaior.test(13,12));
+        System.out.println(verificarSeMaior.test(13,12));
+        System.out.println(verificarSeMaior.test(12,13));
+        System.out.println(verificarSeMaior.test(12,13));
+    }
+}
