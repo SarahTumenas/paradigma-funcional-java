ae92ac662d0b02b5d5eb0cea8669626cf51150f6
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-12-24 08:55:19 -0300

    Function

--- src/main/java/interfacesfuncionais/Funcoes.java
+++ src/main/java/interfacesfuncionais/Funcoes.java
@@ -5,6 +5,8 @@ import java.util.function.Function;
 public class Funcoes {
     public static void main(String[] args) {
         Function<String, String> retornarNomeAoContrario = texto -> new StringBuilder(texto).reverse().toString();
+        Function<String, Integer> converterStringParaInteiroECalcularDobro = string -> Integer.valueOf(string)*2;
         System.out.println(retornarNomeAoContrario.apply("sarah"));
+        System.out.println(converterStringParaInteiroECalcularDobro.apply("20"));
     }
 }
