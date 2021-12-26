dcf431f73c1bd4a2287e6900bd6ab56418e76d34
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-12-26 12:42:49 -0300

    Collection

---
+++ src/main/java/Exercicio/CollectionExemplo.java
@@ -0,0 +1,11 @@
+package Exercicio;
+
+import java.util.Collection;
+import java.util.Set;
+
+public class CollectionExemplo {
+    public static void main(String[] args) {
+        Collection <String> nomes = Set.of("Sarah", "Tumenas", "Marques");
+        System.out.println(nomes);
+    }
+}
