7d48b97183e7a4146477f6cd72066d152d242a15
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-12-26 12:50:40 -0300

    String Repeat

---
+++ src/main/java/Exercicio/StringRepeat.java
@@ -0,0 +1,8 @@
+package Exercicio;
+
+public class StringRepeat {
+    public static void main(String[] args) {
+        String nome = "Vader";
+        System.out.println(nome.repeat(5));
+    }
+}
