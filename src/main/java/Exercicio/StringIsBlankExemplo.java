20d43572ffb776c087986fb287e4dc401e112789
SarahTumenas <sarahtumenas@yahoo.com.br>
2021-12-26 12:47:27 -0300

    Is Blank

---
+++ src/main/java/Exercicio/StringIsBlankExemplo.java
@@ -0,0 +1,8 @@
+package Exercicio;
+
+public class StringIsBlankExemplo {
+    public static void main(String[] args) {
+        String espaco = "";
+        System.out.println(espaco.isBlank());
+    }
+}
